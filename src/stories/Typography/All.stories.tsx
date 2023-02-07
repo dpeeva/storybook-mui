d2d535ac69ac3f2963f24881f54224a39a806823
Desislava <desi.peeva@gmail.com>
2023-02-07 22:46:36 +0200

    add themes index

--- src/stories/Typography/All.stories.tsx
+++ src/stories/Typography/All.stories.tsx
@@ -23,43 +23,43 @@ export default {
 const Template: StoryFn = (args: mui.TypographyProps) => (
     <mui.Stack direction={"column"} spacing={3} py={2}>
         <mui.Typography variant="h1" {...args}>
-            h1. Heading
+            h1. Заглавие на каталога
         </mui.Typography>
         <mui.Typography variant="h2" {...args}>
-            h2. Heading
+            h2. Заглавие на каталога
         </mui.Typography>
         <mui.Typography variant="h3" {...args}>
-            h3. Heading
+            h3. Заглавие на каталога
         </mui.Typography>
         <mui.Typography variant="h4" {...args}>
-            h4. Heading
+            h4. Заглавие на каталога
         </mui.Typography>
         <mui.Typography variant="h5" {...args}>
-            h5. Heading
+            h5. Заглавие на каталога
         </mui.Typography>
         <mui.Typography variant="h6" {...args}>
-            h6. Heading
+            h6. Заглавие на каталога
         </mui.Typography>
         <mui.Typography variant="subtitle1" {...args}>
-            subtitle1. Mapped by default to h6
+            subtitle1. Заглавие на каталога - Mapped by default to h6
         </mui.Typography>
         <mui.Typography variant="subtitle2" {...args}>
-            subtitle2. Mapped by default to h6
+            subtitle2. Заглавие на каталога - Mapped by default to h6
         </mui.Typography>
         <mui.Typography {...args}>
-            body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+            body1. Това е поредица от латински думи които, както са позиционирани , не образувайте изречения с пълен смисъл, а дайте живот на тестов текст, полезен за запълване на пространства, които впоследствие ще бъдат заети от ad hoc текстове, съставени от комуникационни специалисти.
         </mui.Typography>
         <mui.Typography variant="body2" {...args}>
-            body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+            body2. Това е поредица от латински думи които, както са позиционирани , не образувайте изречения с пълен смисъл, а дайте живот на тестов текст, полезен за запълване на пространства, които впоследствие ще бъдат заети от ad hoc текстове, съставени от комуникационни специалисти.
         </mui.Typography>
         <mui.Typography variant="button" {...args}>
-            Button Text
+            Текст върху бутон
         </mui.Typography>
         <mui.Typography variant="caption" {...args}>
-            Caption
+            Пояснителен текст
         </mui.Typography>
         <mui.Typography variant="overline" {...args}>
-            Overline
+            Текст под линия
         </mui.Typography>
     </mui.Stack>
 )
--- src/themes/joy.ts
+++ src/themes/joy.ts
@@ -1,6 +1,10 @@
 import { createTheme } from "@mui/material"
 import { components } from "./components"
 
+const fontFamily = {
+    // fontFamily: ["Merriweather", "sans-serif"].join(", "),
+}
+
 export const joy = createTheme({
     palette: {
         mode: "light",
@@ -28,7 +32,10 @@ export const joy = createTheme({
         },
     },
     typography: {
-        fontFamily: "Montserrat Alternates",
+        allVariants: {
+            color: "#260e1e",
+        },
+        fontFamily: ["Kometa", "Montserrat", "sans-serif"].join(", "),
         body1: {
             fontSize: "18px",
             lineHeight: 1.6,
@@ -37,60 +44,69 @@ export const joy = createTheme({
         body2: {
             fontSize: "16px",
             lineHeight: 1.55,
-            fontWeight: 300,
+            fontWeight: 400,
         },
         h1: {
+            ...fontFamily,
             fontSize: "56px",
             lineHeight: 1.5,
             fontWeight: 600,
         },
         h2: {
+            ...fontFamily,
             fontSize: "48px",
             lineHeight: 1.5,
             fontWeight: 600,
         },
         h3: {
+            ...fontFamily,
             fontSize: "36px",
             lineHeight: 1.5,
             fontWeight: 600,
         },
         h4: {
+            ...fontFamily,
             fontSize: "28px",
             lineHeight: 1.5,
-            fontWeight: 600,
+            fontWeight: 700,
         },
         h5: {
+            ...fontFamily,
             fontSize: "24px",
             lineHeight: 1.5,
-            fontWeight: 600,
+            fontWeight: 700,
         },
         h6: {
+            ...fontFamily,
             fontSize: "18px",
             lineHeight: 1.5,
             fontWeight: 700,
         },
         button: {
-            fontSize: "12px",
+            fontSize: "14px",
             textTransform: "none",
             lineHeight: 1.2,
             fontWeight: 500,
         },
         subtitle1: {
+            ...fontFamily,
             fontSize: "18px",
             lineHeight: 1.5,
             fontWeight: 600,
         },
         subtitle2: {
+            ...fontFamily,
             fontSize: "16px",
             lineHeight: 1.5,
-            fontWeight: 700,
+            fontWeight: 600,
         },
         caption: {
             fontSize: "16px",
             lineHeight: 1,
-            fontWeight: 600,
+            fontWeight: 500,
         },
         overline: {
+            ...fontFamily,
             fontSize: "14px",
             lineHeight: 1,
             fontWeight: 500,
